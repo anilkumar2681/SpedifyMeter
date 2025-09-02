42f4bcdae0d55ec38313ad0fc4dd6aaafe9a4e31
ANIL KUMAR <anilkumar2681@gmail.com>
2025-09-03 03:38:42 +0530

    Init Commit

--- app/src/main/java/com/team42/spedifymeter/ui/theme/Theme.kt
+++ app/src/main/java/com/team42/spedifymeter/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.team42.spedifymeter.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
