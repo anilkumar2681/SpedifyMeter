7d964ffd70eb8532684675570b9b38819384bd80
ANIL KUMAR <anilkumar2681@gmail.com>
2025-09-03 20:49:51 +0530

    Added two screens Splash and Onboarding

---
+++ app/src/main/java/com/team42/spedifymeter/ui/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,8 @@
+package com.team42.spedifymeter.ui.onboarding
+
+/**
+ * Project: Spedify Meter
+ * File: OnBoardingScreen.kt
+ * Created By: ANIL KUMAR on 9/3/2025
+ * Copyright © 2025 Team42. All rights reserved.
+ **/
---
+++ app/src/main/java/com/team42/spedifymeter/ui/splash/SplashScreen.kt
@@ -0,0 +1,8 @@
+package com.team42.spedifymeter.ui.splash
+
+/**
+* Project: Spedify Meter
+* File: SplashScreen.kt
+* Created By: ANIL KUMAR on 9/3/2025
+* Copyright © 2025 Team42. All rights reserved.
+**/
